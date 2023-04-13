7b784e2136a7b68f6ccd654850df3e75b6a9d676
EduardoRdz007 <edurdz010@gmail.com>
2023-04-12 18:39:57 -0600

    FINAL

--- motor.js
+++ motor.js
@@ -1,17 +1,17 @@
 //Creacion montos minimos y maximos
-const generoMasculinoMin = {
+const MasculinoMin = {
    "A": [100, 400, 900, 100, 600],"B": [1000, 600, 1000, 1000, 1000],"C": [400, 200, 200, 1000, 600],"D": [400, 300, 500, 900, 1000]
 }
 
-const generoFemeninoMin = {
+const FemeninoMin = {
    "A": [800, 800, 800, 600, 200],"B": [800, 700, 100, 600, 700],"C": [200, 900, 700, 800, 100],"D": [500, 1000, 600, 400, 700]
 }
 
-const generoMasculinoMax = {
+const MasculinoMax = {
    "A": [4900, 4700, 4600, 4600, 4500],"B": [4700, 4400, 5000, 4400, 4900],"C": [5000, 4700, 5000, 4200, 4600],"D": [4400, 4700, 4300, 4900, 4300]
 }
 
-const generoFemeninoMax = {
+const FemeninoMax = {
    "A": [4000, 4200, 4100, 4200, 4500],"B": [4700, 4200, 4500, 4300, 4400],"C": [4600, 4900, 4600, 4700, 4000],"D": [5000, 4900, 4700, 5000, 4300]
 }
 
@@ -27,18 +27,18 @@ function calculoMotor(tipoNomina, fechaPrimerEmpleo, genero){
    let fechaFormato = [ fechaSplit[1], fechaSplit[0], fechaSplit[2] ].join('/')
    let fechaPrimer = new Date(fechaFormato)
    
-   let diferenciaMeses = monthDiff(fechaPrimer, fechaActual)
-   let indexTablaMeses
-   if (diferenciaMeses <= 26){
-      indexTablaMeses = 0
-   }else if (diferenciaMeses == 27){
-      indexTablaMeses = 1
-   }else if (diferenciaMeses == 28){
-      indexTablaMeses = 2
-   }else if (diferenciaMeses == 29){
-      indexTablaMeses = 3
+   let Meses = monthDiff(fechaPrimer, fechaActual)
+   let TablaMeses
+   if (Meses <= 26){
+      TablaMeses = 0
+   }else if (Meses == 27){
+      TablaMeses = 1
+   }else if (Meses == 28){
+      TablaMeses = 2
+   }else if (Meses == 29){
+      TablaMeses = 3
    }else{
-      indexTablaMeses = 4
+      TablaMeses = 4
    }
    
    function resultado(montoMinimoRes, montoMáximoRes){
@@ -52,23 +52,23 @@ function calculoMotor(tipoNomina, fechaPrimerEmpleo, genero){
    //Determinar creditos minimos y maximos 
    if(genero == "f"){
       if(tipoNomina == "A"){
-         return resultado(generoFemeninoMin.A[indexTablaMeses], generoFemeninoMax.A[indexTablaMeses])
+         return resultado(FemeninoMin.A[TablaMeses], FemeninoMax.A[TablaMeses])
       }else if(tipoNomina == "B"){
-         return resultado(generoFemeninoMin.B[indexTablaMeses], generoFemeninoMax.B[indexTablaMeses])
+         return resultado(FemeninoMin.B[TablaMeses], FemeninoMax.B[TablaMeses])
       }else if(tipoNomina == "C"){
-         return resultado(generoFemeninoMin.C[indexTablaMeses], generoFemeninoMax.C[indexTablaMeses])
+         return resultado(FemeninoMin.C[TablaMeses], FemeninoMax.C[TablaMeses])
       }else{
-         return resultado(generoFemeninoMin.D[indexTablaMeses], generoFemeninoMax.D[indexTablaMeses])
+         return resultado(FemeninoMin.D[TablaMeses], FemeninoMax.D[TablaMeses])
       }
    }else{
       if(tipoNomina == "A"){
-         return resultado(generoMasculinoMin.A[indexTablaMeses], generoMasculinoMax.A[indexTablaMeses])
+         return resultado(MasculinoMin.A[TablaMeses], MasculinoMax.A[TablaMeses])
       }else if(tipoNomina == "B"){
-         return resultado(generoMasculinoMin.B[indexTablaMeses], generoMasculinoMax.B[indexTablaMeses])
+         return resultado(MasculinoMin.B[TablaMeses], MasculinoMax.B[TablaMeses])
       }else if(tipoNomina == "C"){
-         return resultado(generoMasculinoMin.C[indexTablaMeses], generoMasculinoMax.C[indexTablaMeses])
+         return resultado(MasculinoMin.C[TablaMeses], MasculinoMax.C[TablaMeses])
       }else{ 
-         return resultado(generoMasculinoMin.D[indexTablaMeses], generoMasculinoMax.D[indexTablaMeses])
+         return resultado(MasculinoMin.D[TablaMeses], MasculinoMax.D[TablaMeses])
       }
    }
 }
@@ -82,7 +82,7 @@ function monthDiff(mes1, mes2) {
    return months <= 0 ? 0 : months;
 }
 
-console.log(calculoMotor("A", "12/06/2022", "f"))
-console.log(calculoMotor("B", "30/12/1993", "f"))
-console.log(calculoMotor("C", "19/09/2020", "m"))
-console.log(calculoMotor("D", "15/01/2019", "m"))
+console.log(calculoMotor("A", "19/01/2021", "m"))
+console.log(calculoMotor("B", "20/11/1983", "f"))
+console.log(calculoMotor("A", "19/09/2020", "m"))
+console.log(calculoMotor("D", "12/01/2011", "m"))
